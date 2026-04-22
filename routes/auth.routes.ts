d345c6bf6c6c065a59de2c9356753bba1d8f5bf5
MrCLET <amlaganclement@gmail.com>
2026-04-22 21:43:25 +0000

    Auth routes creation

--- routes/auth.routes.ts
+++ routes/auth.routes.ts
@@ -1,12 +1,20 @@
 import { Router } from 'express';
-import { login, register } from '../controllers/auth.controller';
+import { login, register ,refresh , logout } from '../controllers/auth.controller';
 
 const router = Router();
 
-// User login
-router.post('/login', login);
+// User login & Token Generate
+router.post('/auth/login', login);
 
 // User register
-router.post('/register', register);
+router.post('/auth/register', register);
+
+// Token Refreshing
+router.post('auth/refresh', refresh);
+
+// Token broker
+router.post('auth/logout', logout);
+
+
 
 export default router;
