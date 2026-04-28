66f82ad3312a15b8425797f5c2a11d15adfbf61d
beni <y6502192@gmail.com>
2026-04-28 20:04:24 +0000

    creation des logiques des properties dans le controllers

--- controllers/properties.controller.ts
+++ controllers/properties.controller.ts
@@ -1,32 +1,112 @@
+import { privateDecrypt } from 'crypto';
+import { NOTFOUND } from 'dns';
 import { Request, Response } from 'express';
+import Properties from 'models/Properties';
+import { title } from 'process';
+import { escape } from 'querystring';
+import { uploadPicture } from './users.controller';
 
 export async function createProperty(req: Request, res: Response) {
-    return res.status(201).json({ success: true, message: "Property created" });
+    try {
+        const{ ownerId, title, price, location } = req.body;
+        //
+        const property = await Properties.find.findById(ownerId);
+
+        if (!property) {
+            return res.status(400).json ({ message: "no acces to property"});
+        }
+        const properties = await Properties.create({
+            ownerId,
+            title,
+            price,
+            location
+        });
+         return res.status(201).json({ success: true, message: "Property created" });
+    } catch (error: any) {
+        res.status(500).json({ message: error.message });
+    }
 }
+
 
 export async function getAllProperties(req: Request, res: Response) {
-    return res.status(200).json({ success: true, properties: [] });
+    try {
+        const properties = await Properties.getAll({
+        title,
+        location
+        });
+        return res.status(200).json({ success: true, properties: [] });
+    } catch (error: any) {
+        res.status(501).json({ message: NOTFOUND })
+    }
 }
+
+
 
 export async function getPropertyDetails(req: Request, res: Response) {
+    try {
+        const properties = await Properties.details();
     return res.status(200).json({ success: true, property: {} });
+    } catch (error: any) {
+        res.status(500).json({ message: NOTFOUND })
+    }
 }
+
+
+
 
 export async function updateProperty(req: Request, res: Response) {
+    try {
+        const properties = await Properties.update({
+            title,
+            location,
+            Location
+         });
     return res.status(200).json({ success: true, message: "Property updated" });
+    } catch (error: any) {
+        res.status(500).json({ message: error})
+    }
 }
+
+
 
 export async function deleteProperty(req: Request, res: Response) {
+    try {
+        const properties = await Properties.delete({
+            title,
+            location,
+        });
     return res.status(200).json({ success: true, message: "Property deleted" });
+    } catch (error: any) {
+        res.status(500).json({ message: error})
+    }
 }
+
+
 
 export async function uploadPropertyImages(req: Request, res: Response) {
+    try {
+        const properties = await Properties.upload.images();
     return res.status(200).json({ success: true, message: "Images uploaded" });
+    } catch (error: any) {
+        res.status(500).json({ message: error})
+    }    
 }
 
+
+
 export async function getMyProperties(req: Request, res: Response) {
+    try {
+        const properties = await Properties.getMy({
+            title,
+            uploadPicture,
+        });
     return res.status(200).json({ success: true, properties: [] });
+    } catch (error: any) {
+        res.status(500).json({ message: "error"})
+    }
 }
+
+
 
 export async function getOwnerProperties(req: Request, res: Response) {
     return res.status(200).json({ success: true, properties: [] });
