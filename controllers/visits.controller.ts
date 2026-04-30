295674d2e1f72d17c2d9c537e0d71c04ac693242
BELO <belosekou@gmail.com>
2026-04-30 22:38:55 +0200

    xxxxxx

--- controllers/visits.controller.ts
+++ controllers/visits.controller.ts
@@ -3,9 +3,9 @@ import { AuthRequest } from '../middlewares/authMiddleware';
 import Properties from "../models/Properties";
 import Visits from "../models/Visits";
 
-// Create Visits
+// Creat Visits
 export async function createVisits(req: AuthRequest, res: Response) {
-    try { 
+    try {
         const { propertyId, visitDate, message } = req.body;
 
         // récupérer la propriété
